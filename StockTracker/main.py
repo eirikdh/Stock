144406f3e055a7cd3478e1c3edb5c4ca62e3d934
eirikdh <eirikdh@gmail.com>
2024-09-26 08:50:26 +0200

    Update main.py

--- StockTracker/main.py
+++ StockTracker/main.py
@@ -42,7 +42,7 @@ NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 
 def fetch_all_stock_symbols():
     try:
-        with open('all_tickers.txt', 'r') as file:
+        with open('StockTracker/all_tickers.txt', 'r') as file:
             return [line.strip() for line in file if line.strip()]
     except FileNotFoundError:
         logger.warning("all_tickers.txt not found. Using fallback symbols.")
